86825e2a28482f35583c13a4d40af20a1676c55c
Mayco Lazos <maycolazos25@gmail.com>
2023-02-11 18:13:57 -0300

    first commit

---
+++ js/unidad7.js
@@ -0,0 +1,11 @@
+alert("Bienvenido!")
+
+var nombreUsuario = prompt("¿Como es tu Nombre")
+
+var prenda = prompt('¿Que prenda esta buscando?')
+
+var sexo = prompt("¿De hombre, mujer o niño?")
+
+var talle = prompt("¿Que talle?")
+
+var color = prompt("¿De que color")
---
+++ js/unidad8.js
@@ -0,0 +1,14 @@
+
+
+function agrandar(img) {
+    img.style.width = "150%";
+    img.style.height = "200%";
+    
+}
+
+function achicar(img) {
+    img.style.width = "100%";
+    img.style.height = "100%";
+    
+    
+}
